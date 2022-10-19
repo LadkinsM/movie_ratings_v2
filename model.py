1df0db7b39bd81189940ab818e544ceae2ada311
Mauri Adkins <mauriadkins@gmail.com>
2022-10-18 23:02:20 -0500

    Part One Completed

--- model.py
+++ model.py
@@ -1,11 +1,60 @@
 """Models for movie ratings app."""
 
+from collections import UserString
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
 
-# Replace this with your code!
+class User(db.Model):
+    """User"""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+
+    ratings = db.relationship("Rating", back_populates="user")
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
+
+
+class Movie(db.Model):
+    """Movie"""
+
+    __tablename__ = "movies"
+
+    movie_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    overview = db.Column(db.Text, nullable=False)
+    release_date = db.Column(db.DateTime, nullable=False)
+    poster_path = db.Column(db.String, nullable=False)
+
+    ratings = db.relationship("Rating", back_populates="movie")
+
+    def __repr__(self):
+        return f'<Movie movie_id={self.movie_id} title={self.title}>'
+
+class Rating(db.Model):
+    """Rating"""
+
+    __tablename__ = "ratings"
+
+    rating_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    score = db.Column(db.Integer, nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey("movies.movie_id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+
+    movie = db.relationship("Movie", back_populates="ratings")
+    user = db.relationship("User", back_populates="ratings")
+
+    def __repr__(self):
+        return f'<Rating rating_id={self.rating_id} score={self.score}'
+
+
 
 
 def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
@@ -27,3 +76,5 @@ if __name__ == "__main__":
     # query it executes.
 
     connect_to_db(app)
+
+
